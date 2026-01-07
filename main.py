48b96180bca6f151187651cd7e27743467fcc333
Emil <kitanarau@yandex.ru>
2026-01-08 02:09:07 +0500

    reports done

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from src.views import main_page
 from src.services import prepare_transactions, investment_bank
+from src.utils import open_xlsx
+from src.reports import spending_by_category
 
 if __name__ == "__main__":
-    result_views = main_page(input("Введите строку с датой и временем в формате YYYY-MM-DD HH:MM:SS"))
-    print(result_views)
+    #result_views = main_page(input("Введите строку с датой и временем в формате YYYY-MM-DD HH:MM:SS"))
+    #print(result_views)
 
-    a = investment_bank("2021-12", prepare_transactions(), 50)
-    print(a)
+    #result_services = investment_bank("2021-12", prepare_transactions(), 50)
+    #print(result_services)
+
+
+    result_reports = spending_by_category(open_xlsx(), "Супермаркеты", "2021-12-31 21:15:58")
+    print(result_reports)
